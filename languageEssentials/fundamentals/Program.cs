754e8c34c45d01d6b959b24b315ab3e827e99940
mchorney <m.chorney7@gmail.com>
2018-01-08 14:26:42 -0600

    added C# assignments

---
+++ languageEssentials/fundamentals/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        // Print all the valeus between 1- 255
+        //  for(int i = 1; i < 256; i++)
+        //     {
+        //         Console.WriteLine(i);       
+        //     }
+
+
+        //Print values divisible by 3 and 5 but not both
+        /* 
+        for(int i = 1; i < 101; i ++)
+            {
+                if(!(i % 5 == 0 && i % 3 == 0))
+                {
+                     if(i % 5 == 0 || i % 3 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            } 
+        */
+        for(int i = 1; i < 101; i ++)
+            {
+                System.Console.WriteLine(i);
+                if(i % 5 == 0 && i % 3 == 0)
+                {
+                     Console.WriteLine("FizzBuzz");
+                }
+                else if(i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if(i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+            }
+        }
+    }
+}
