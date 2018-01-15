2a4ffbd5026d3653969ccb3f3973fae6a8e56f14
mchorney <m.chorney7@gmail.com>
2018-01-15 14:33:24 -0600

    added ASP.NETCore assignments

---
+++ ASP.NETCore/passcode/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace randomPasscode.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            int? attempt = HttpContext.Session.GetInt32("attempt");
+            if(attempt == null)
+            {
+                HttpContext.Session.SetInt32("attempt", 1);
+            }
+            else
+            {
+                attempt += 1;
+                HttpContext.Session.SetInt32("attempt", (int)attempt);
+            }
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            string passcode = "";
+            Random rand = new Random();
+            for(int i = 1; i < 15; i++)
+            {
+                int x = rand.Next(0,36);
+                passcode += chars[x];
+            }
+            TempData["passcode"] = passcode;
+            TempData["attempt"] = HttpContext.Session.GetInt32("attempt");
+            
+            return View();
+        }
+    }
+}
